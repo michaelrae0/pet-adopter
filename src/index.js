a7cd00f4eb6215da3219a9772d5851e7c60a4ed6
mdrae <mdrae0@gmail.com>
2019-04-05 16:33:26 -0500

    Prepping workflow
    Added sass. Merged app.js into index.js.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <a href='https://www.petfinder.com/developers/'>Pet finder API</a>
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
