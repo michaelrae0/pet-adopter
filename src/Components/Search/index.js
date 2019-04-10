0ad82fb6d01fe99742a47dacb961166821fb5ae0
mdrae <mdrae0@gmail.com>
2019-04-09 19:25:02 -0500

    Basic form styles created
    Forms are easier to look at. Not finished but looks closer to the goal.

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,100 +1,53 @@
 import React from 'react'
+import _cn from 'classnames'
 
-import { Link } from "react-router-dom";
-import classnames from 'classnames'
+import AnimalForm from '../AnimalForm/index'
+import ShelterForm from '../ShelterForm/index'
 
 import * as search from './search.module.scss'
-import api from '../../util/apiClient'
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      type: 'dog',
-      breed: '',
-      breeds: ['Loading...'],
-      location: '',
-
-      redirect: false,
+      form: 'animal'
     }
-  }
-  
-  componentDidMount() {
-    this.setBreeds(this.state.type);
-  }
-
-  setBreeds = type => {
-    this.setState({ breeds: ['Loading...'] })
-
-    api.breeds( {type} )
-      .then( ({data}) => {
-        const breeds = data.breeds.map( breed => breed.name);
-        this.setState({ breeds })
-      })
   }
 
   render() {
-    const { type, breed, breeds, location } = this.state
+    const { form } = this.state
 
-    const breedOptions = breeds.map( breed => (
-      <option value={breed} key={breed} >{breed}</option> 
-    ));
+    let animalStyles = {};
+    let shelterStyles = {};
+
+    if (form === 'animal') {
+      animalStyles = _cn(search.animal_selector, search.selected)
+      shelterStyles = _cn(search.shelter_selector)
+    } else {
+      shelterStyles = _cn(search.shelter_selector, search.selected)
+      animalStyles = _cn(search.animal_selector)
+    }
 
     return (
-      <div className={classnames(search.container)}>
-        <form
-          action=''
-          method='POST'
-          onSubmit={this.submitForm}
-        >
-          <label htmlFor='type' >Type</label>
-          <select 
-            type='select' name='type' id='type'
-            className={classnames(search.form_info, search.type)}
-            onChange={ e => {
-              this.setBreeds(e.target.value);
-              this.setState({ type: e.target.value });
-            }}
+      <div className={_cn(search.container)}>
+
+        <div  className={_cn(search.selector_cont)}>
+          <div
+          className={animalStyles}
           >
-            <option value='dog'>Dog</option>
-            <option value='cat'>Cat</option>
-            <option value='bird'>Bird</option>
-            <option value='rabbit'>Rabbit</option>
-            <option value='small & furry'>Small & Furry</option>
-            <option value='scales, fins & other'>Fish & Reptiles</option>
-            <option value='horse'>Horse</option>
-            <option value='barnyard'>Barnyard</option>
-          </select>
-          <label htmlFor='breed' >Breed</label>
-          <select 
-            placeholder='Boxer' type='select' name='breed' id='breed'
-            className={classnames(search.form_info, search.breed)}
-            onChange={ e => this.setState({ breed: e.target.value }) }
-          >
-            <option value='' ></option>
-            {breedOptions}
-          </select>
-          <label htmlFor='location' >Location</label>
-          <input
-            placeholder='Atlanta, GA' type='text' name='location' id='location'
-            className={classnames(search.form_info, search.location)}
-            value={location}
-            onChange={ e => this.setState({ location: e.target.value }) }
-          />
-          <Link
-            to={{
-              pathname: "/results",
-              state: { type, breed, location }
-            }} 
-          >
-            <div
-              className={classnames(search.btn)}
-            >
-              <span>Submit</span>
-            </div>
-          </Link>
-        </form>
+            <p>Animals</p>
+          </div>
+          <div className={shelterStyles}>
+            <p>Shelters</p>
+          </div>
+        </div>
+
+        <div className={_cn(search.form_cont)}>
+          {(form === 'animal') && <AnimalForm />}
+          {(form === 'shelter') && <ShelterForm />}
+        </div>
+        
       </div>
     )
   }
