58abc529717ffbc1957fef6c9f5a32dd7e602a5f
mdrae <mdrae0@gmail.com>
2019-04-26 15:36:37 -0500

    Uses layout components in Header
    This commit uses the layout components in the header so it has consistent
    styling with the rest of the site.

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import classnames from 'classnames'
 
 import * as header from './header.module.scss'
+import Container from '../Container'
+import Row from '../Row'
 
 class Header extends React.Component {
   constructor(props) {
@@ -19,28 +21,30 @@ class Header extends React.Component {
   
   render() {
     return (
-      <div className={classnames(header.navbar)}>
-        <div className={classnames(header.link_section)}>
-          <Link to='/'>
-            <div
-              className={classnames(header.image_container)}
-              onClick={this.handleOnClick}
-            >
-              <img 
-                src='http://placekitten.com/60/60' 
-                alt='' 
-                className={header.image}
-              />
+      <section className={header.section}>
+        <Container>
+          <Row className={header.row}>
+            <Link to='/'>
+              <div
+                className={classnames(header.image_container)}
+                onClick={this.handleOnClick}
+              >
+                <img 
+                  src='http://placekitten.com/60/60' 
+                  alt='' 
+                  className={header.image}
+                />
+              </div>
+            </Link>
+            <div className={header.categories}>
+              <h2 className={header.link} >All</h2>
+              <h2 className={header.link} >Dogs</h2>
+              <h2 className={header.link} >Cats</h2>
+              <h2 className={header.link} >Search</h2>
             </div>
-          </Link>
-          <div className={header.categories}>
-            <h2 className={header.link} >All</h2>
-            <h2 className={header.link} >Dogs</h2>
-            <h2 className={header.link} >Cats</h2>
-            <h2 className={header.link} >Search</h2>
-          </div>
-        </div>
-      </div>
+          </Row>
+        </Container>
+      </section>
       )
   }
 }
