5531ff8b9603129ad003009084c91260dd94bbba
mdrae <mdrae0@gmail.com>
2019-04-27 23:30:57 -0500

    Add styles to Detail components
    This commit adds basic styles to AnimalDetails and ShelterDetails to give
    the information a sense of heirarchy. The components have different data
    but similar styling, so the stylesheet is imported from the Details page.

--- src/Components/ShelterDetails/index.js
+++ src/Components/ShelterDetails/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
-import * as shelterDetails from './shelterDetails.module.scss'
+import * as details from '../../pages/Details/details.module.scss'
+import Container from '../Container'
+import Row from '../Row'
+import { H1, H2, H3, H4 , H5 , H6, SectionBody } from '../Typography'
 
 const ShelterDetails = ({ info }) => {
   const {
@@ -13,17 +16,29 @@ const ShelterDetails = ({ info }) => {
   } = info;
 
   return (
-    <div className={shelterDetails.text_cont}>
-      <h2>{name}</h2>
-      <p>{mission_statement}</p>
-      <p>{adoption.policy}</p>
-      <div>
-        <h4>Contact Info</h4>
-        <p>Address: {address.address1}, {address.city}, {address.state}</p>
-        <p>Phone: {phone}</p>
-        <p>Email: {email}</p>
-      </div>
-    </div>
+    <section className={details.section}>
+      <Container restricted >
+        <Row className={details.row}>
+
+          <div className={details.primary} >
+            <H1 className={details.title} text={name} />
+            <SectionBody className={details.statement} text={mission_statement} />
+            <SectionBody text={adoption.policy} />
+          </div>
+
+          <div className={details.secondary}>
+            <div className={details.info_type}>
+              <H2 className={details.subtitle} text={`Contact Info`} />
+              <div className={details.detail_group} >
+                <SectionBody text={`Address: ${address.address1}, ${address.city}, ${address.state}`}/>
+                <SectionBody text={`Phone: ${phone}`}/>
+                <SectionBody text={`Email: ${email}`}/>
+              </div>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
