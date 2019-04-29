8349f70010145ed900fc591994bc39c76ab43e62
mdrae <mdrae0@gmail.com>
2019-04-29 18:41:31 -0500

    Adds a preview row to Carousel
    This commit removes the left and right arrows from the carousel and
    replaces them with a row of thumbnails that can be clicked to navigate
    through the images.

--- src/Components/Carousel/index.js
+++ src/Components/Carousel/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import classnames from 'classnames'
 
 import * as cara from './carousel.module.scss'
-import Container from '../Container'
 import Row from '../Row'
 import { ReactComponent as ArrowSVG } from '../../images/Arrow.svg'
 
@@ -11,25 +10,33 @@ export default class Carousel extends React.Component {
     super(props);
 
     this.state = {
-      fullSizedPhotos: [],
+      caraPhotos: [],
       currentIndex: 0,
       backAvailable: false,
       nextAvailable: false
     }
   }
   
-  componentDidMount() {
-    let fullSizedPhotos = [];
+  componentWillMount() {
+    let caraPhotos = [];
 
     if (!this.props.photos[0]) {
-      fullSizedPhotos = ['https://via.placeholder.com/900x700.png?text=No+Image+Available'];
+      caraPhotos = [{
+        full: 'https://via.placeholder.com/900x700.png?text=No+Image+Available',
+        medium: 'https://via.placeholder.com/900x700.png?text=No+Image+Available',
+      }];
     }
     else {
-      fullSizedPhotos = this.props.photos.map( photo => photo.full )
+      caraPhotos = this.props.photos;
     }
 
-    this.arrowAvailability(0, fullSizedPhotos);
-    this.setState({ fullSizedPhotos });
+    this.setState({ caraPhotos });
+
+    this.arrowAvailability(0, caraPhotos);
+  }
+
+  componentDidMount() {
+    this.handlePreviewClick(0);
   }
 
   arrowAvailability = (index, photos) => {
@@ -54,41 +61,76 @@ export default class Carousel extends React.Component {
   }
 
   handleOnClick = next => {
-    const { currentIndex, fullSizedPhotos } = this.state;
+    const { currentIndex, caraPhotos } = this.state;
 
     if (next === false) {
-      this.arrowAvailability(currentIndex - 1, fullSizedPhotos);
+      this.arrowAvailability(currentIndex - 1, caraPhotos);
     }
     if (next === true) {
-      this.arrowAvailability(currentIndex + 1, fullSizedPhotos);
+      this.arrowAvailability(currentIndex + 1, caraPhotos);
     }
+  }
+
+  handlePreviewClick = (i, event = '') => {
+    if (event) event.preventDefault();
+    const previews = document.querySelector(`.${cara.previews}`);
+    const offset = ((this.state.caraPhotos.length - 1) - (i * 2 )) ;
+    const previewWidth = 180 + (16 * 0.15 * 2);
+    const padding = offset * previewWidth;
+
+    if (padding >= 0) {
+      previews.style.paddingLeft = padding + 'px';
+      previews.style.paddingRight = 0 + 'px';
+    } else {
+      previews.style.paddingLeft = 0 + 'px';
+      previews.style.paddingRight = -padding + 'px';
+    }
+
+
+    this.setState({
+      currentIndex: i,
+    })
   }
   
   render() {
-    const { fullSizedPhotos, currentIndex, backAvailable, nextAvailable } = this.state;
+    const { caraPhotos, currentIndex, backAvailable, nextAvailable } = this.state;
 
-    // Navigation buttons won't render if currentIndex is 0 or length-1
     return (
       <section className={cara.section}>
-        <Container>
-          <Row className={cara.row}>
-            <div className={cara.carousel}>
-              <img src={fullSizedPhotos[currentIndex]} alt='' className={cara.image} />
-              {backAvailable &&
-              <div className={classnames(cara.btn, cara.btn__back)} onClick={() => this.handleOnClick(false)} >
-                <div className={cara.arrow_container}>
-                  <ArrowSVG className={classnames(cara.arrow, cara.arrow_back)} key={2} />
-                </div>
-              </div>}
-              {nextAvailable &&
-              <div className={classnames(cara.btn, cara.btn__next)} onClick={() => this.handleOnClick(true)} >
-                <div className={cara.arrow_container}>
-                  <ArrowSVG className={classnames(cara.arrow, cara.arrow_next)} />
-                </div>
-              </div>}
+        <Row className={cara.row}>
+          <div className={cara.carousel}>
+            <img src={caraPhotos[currentIndex].full} alt='' className={cara.image} />
+            {/* {backAvailable &&
+            <div className={classnames(cara.btn, cara.btn__back)} onClick={() => this.handleOnClick(false)} >
+              <div className={cara.arrow_container}>
+                <ArrowSVG className={classnames(cara.arrow, cara.arrow_back)} key={2} />
+              </div>
+            </div>}
+            {nextAvailable &&
+            <div className={classnames(cara.btn, cara.btn__next)} onClick={() => this.handleOnClick(true)} >
+              <div className={cara.arrow_container}>
+                <ArrowSVG className={classnames(cara.arrow, cara.arrow_next)} />
+              </div>
+            </div>} */}
+          </div>
+        </Row>
+        <div className={cara.preview_container}>
+          <Row className={cara.preview_row} noMargin>
+            <div className={cara.previews} >
+              {caraPhotos.map( (photo, i) => {
+                
+                return (
+                  <div
+                    className={classnames(cara.preview_image_container, {[cara.preview_image_container__active]: i === currentIndex})}
+                    onClick={e => this.handlePreviewClick(i, e)}
+                  >
+                    <img src={photo.medium} alt='' className={cara.preview_image} />
+                  </div>
+                )
+              })}
             </div>
           </Row>
-        </Container>
+        </div>
       </section>
     )
   }
