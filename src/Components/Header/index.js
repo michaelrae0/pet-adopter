d74dcba5e0d2369ddf2cfbef95efdcceccc05389
mdrae <mdrae0@gmail.com>
2019-05-04 22:59:34 -0500

    Adds mobile styles to Header
    This commit switches designs when below the `min-tablets` breakpoint. A
    search icon appears that can be clicked to bring up a search box.
    
    This commit also changes styles when the header is fixed.

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -7,6 +7,7 @@ import Container from '../Container'
 import Row from '../Row'
 import logo from '../../images/paw-icon.png'
 import SearchBar from '../SearchBar'
+import { ReactComponent as SearchSVG } from '../../images/Search.svg'
 import { H2 } from '../Typography'
 
 export default class Header extends React.Component {
@@ -16,21 +17,26 @@ export default class Header extends React.Component {
     this.state = {
       redirect: false,
       isNavFixed: false,
+      isNavActive: false,
     }
   }
 
   componentDidMount() {
     window.addEventListener('scroll', this.fixSiteHeader);
+    window.addEventListener('resize', this.deactivateSiteHeader);
   }
 
   componentWillUnmount() {
     window.removeEventListener('scroll', this.fixSiteHeader);
+    window.addEventListener('resize', this.deactivateSiteHeader);
   }
 
   fixSiteHeader = () => {
     const siteHeader = document.querySelector(`.${header.site__header}`);
-    if (window.scrollY >= siteHeader.offsetHeight) { // window y >= header height
-      document.body.style.paddingTop = siteHeader.offsetHeight + 'px';
+    const breakpoint = window.innerWidth >= 993 ? 90 : 60; // 993 == min-tablet
+
+    if (window.scrollY >= breakpoint) { // window y >= header height
+      document.body.style.paddingTop = breakpoint + 'px';
       siteHeader.classList.add(header.state_fixed);
       this.setState({
         isNavFixed: true,
@@ -43,8 +49,26 @@ export default class Header extends React.Component {
       })
     }
   }
+
+  deactivateSiteHeader = () => {
+    if (window.innerWidth >= 993) {
+      this.setState({
+        isNavActive: false,
+      })
+    }
+  }
+
+  handleClick = e => {
+    this.setState({
+      isNavActive: !this.state.isNavActive,
+    })
+    e.preventDefault();
+  }
   
   render() {
+    const { isNavActive } = this.state;
+    console.log(isNavActive)
+
     return (
       <section className={header.site__header}>
         <Container className={header.container}>
@@ -62,8 +86,11 @@ export default class Header extends React.Component {
                 <H2 text={`Adopt a Pet`} className={header.site_name} />
               </div>
             </Link>
-            <div className={header.box__right }>
-              <SearchBar />
+            <div className={header.box__right } >
+              <SearchSVG className={header.search_icon} onClick={this.handleClick}/>
+              <div className={classnames(header.search_container, {[header.search_container__active]: isNavActive})}>
+                <SearchBar className={classnames({[header.search_bar__active]: isNavActive})} />
+              </div>
             </div>
           </Row>
         </Container>
